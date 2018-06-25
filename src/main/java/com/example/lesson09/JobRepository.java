201cd486c0cae040c4efbb86edd899b25db2fcc6
nosukja99 <nosukja99@gmail.com>
2018-06-25 16:03:27 -0400

    Initial commit

---
+++ src/main/java/com/example/lesson09/JobRepository.java
@@ -0,0 +1,6 @@
+package com.example.lesson09;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface JobRepository extends CrudRepository<Job, Long>{
+}
---
+++ src/main/java/com/example/lesson09/Lesson09Application.java
@@ -0,0 +1,12 @@
+package com.example.lesson09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson09Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lesson09Application.class, args);
+	}
+}
